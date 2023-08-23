edd88882b05deac6318d954ba3d239916aa81d38
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2023-08-23 14:07:21 +0200

    [exercise 19] Add galaxy

--- 19-galaxy-generator/exercise/src/script.js
+++ 19-galaxy-generator/exercise/src/script.js
@@ -19,7 +19,65 @@ const scene = new THREE.Scene()
 /**
  * Galaxy
  */
+const parameters = {}
+parameters.count = 1000
+parameters.size = 0.02
+parameters.radius = 5
 
+let geometry = null // We need to declare these variables outside the function so we can access them from the gui
+let material = null // We need to declare these variables outside the function so we can access them from the gui 
+let points = null // We need to declare these variables outside the function so we can access them from the gui
+ 
+const generateGalaxy = () => 
+{
+    // Remove old galaxy
+    if(points !== null)
+    {
+        geometry.dispose() // We need to dispose the geometry because it's a custom geometry
+        material.dispose() // We need to dispose the material because it's a custom material
+        scene.remove(points) // Remove the points from the scene there no such thing as scene.remove(geometry) or scene.remove(material)
+    }
+
+    geometry = new THREE.BufferGeometry()
+
+    const positions = new Float32Array(parameters.count * 3)
+
+    for(let i = 0; i < parameters.count; i++)
+    {
+        const i3 = i * 3
+
+        positions[i3 + 0] = (Math.random() - 0.5) * 3 // -0.5 to center the galaxy x axis
+        positions[i3 + 1] = (Math.random() - 0.5) * 3 // -0.5 to center the galaxy y axis
+        positions[i3 + 2] = (Math.random() - 0.5) * 3 // -0.5 to center the galaxy z axis
+    }
+
+    geometry.setAttribute(
+        'position', 
+        new THREE.BufferAttribute(positions, 3) // 3 = number of values per vertex
+    )
+
+    /**
+     * Material
+     */
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending // Additive blending is used for particles
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+
+generateGalaxy()
+
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
